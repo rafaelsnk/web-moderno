7477cddcec49657404bf9c75f5e3f0084217a4d3
Rafael <83092886+rafaelsnk@users.noreply.github.com>
2023-02-10 16:33:11 -0300

    new update

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,11 @@
+const pessoa = {
+  nome: 'Ana',
+  idade: 5,
+  endereco: {
+    logradouro: 'Rua ABC',
+    numero: 1000
+  }
+}
+
+const { nome, idade } = pessoa // extraindo nome e idade do obj pessoa
+console.log(nome, idade)
---
+++ fundamentos/destructuring3.js
@@ -0,0 +1,8 @@
+function rand({ min = 0, max = 1000 }) {
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
+}
+
+console.log(rand({ max: 50, min: 10 }));
+console.log(rand({ min: 990 }));
+console.log(rand({}));
