8d5c7c5943ec9086b7989286722e14d655686cdf
Rafael <83092886+rafaelsnk@users.noreply.github.com>
2023-03-06 14:51:29 -0300

    update

---
+++ funcao/IIFE.js
@@ -0,0 +1,5 @@
+//função autoinvocada
+
+setInterval(function () {
+  console.log('Será iniciada na hora')
+}, 1000)()
---
+++ funcao/factory2.js
@@ -0,0 +1,11 @@
+// produto e valor
+
+function criarProduto(nome, preco) {
+  return {
+    nome,
+    preco,
+    desconto: 0.1
+  }
+}
+
+console.log(criarProduto('Lápis', 0.99))
